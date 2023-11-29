6af8d10d412ee617ae9fd51db3290dd7f8ba1bd3
KirillDmit <kirilldmitriev20015@gmail.com>
2023-11-29 20:55:58 +0500

    Init

---
+++ src/main/java/com/example/extraactivities/ExtraActivitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.extraactivities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraActivitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtraActivitiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/extraactivities/ExtraActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extraactivities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraActivitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
